06fbb41f046c12577211b829aed73de60a45333d
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-12-05 17:00:22 +0100

    Add tests project

---
+++ TheConsultancyFirm.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheConsultancyFirm.Controllers;
+using Xunit;
+
+namespace TheConsultancyFirm.Tests.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void About()
+        {
+            var controller = new HomeController();
+            var result = controller.About();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            var message = viewResult.ViewData["Message"];
+            Assert.Equal("Your application description page.", message);
+        }
+    }
+}
