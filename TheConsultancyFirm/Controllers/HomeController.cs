792c53601f7f4ce7fbc1ab76517cb18c2810a4b4
KevinHoudts <kvhoudts@hotmail.com>
2017-12-12 10:58:56 +0100

    Eerste opzetje Home pagina
    De about en Contact action edits verwijderd. Is hier niet nodig.

--- TheConsultancyFirm/Controllers/HomeController.cs
+++ TheConsultancyFirm/Controllers/HomeController.cs
@@ -10,22 +10,12 @@ namespace TheConsultancyFirm.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Haalt met GET de homepagina op
+        /// </summary>
+        /// <returns>De homepagina -> Index.cshtml</returns>
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
