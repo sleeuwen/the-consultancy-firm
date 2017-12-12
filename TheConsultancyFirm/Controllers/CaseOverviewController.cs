5b4ce18acdd0c55d3466f98ff16aced65eeaaa81
Unknown <kvanzeijl@hotmail.com>
2017-12-12 12:51:17 +0100

    WIP

---
+++ TheConsultancyFirm/Controllers/CaseOverviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheConsultancyFirm.Controllers
+{
+	public class CaseOverviewController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
