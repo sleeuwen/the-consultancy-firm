71aa9dae3b6a117336b8dae32902fdc4fa15109f
Unknown <kvanzeijl@hotmail.com>
2017-12-13 18:52:31 +0100

    WIP

--- TheConsultancyFirm/Startup.cs
+++ TheConsultancyFirm/Startup.cs
@@ -42,7 +42,7 @@ namespace TheConsultancyFirm
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=News}/{action=Index}/{id?}");
             });
         }
     }
