36307a1a0fdf30187998bc7ab8bb076fa734d207
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-12-16 15:04:34 +0100

    Remove whitespace-only changes

--- TheConsultancyFirm/Controllers/HomeController.cs
+++ TheConsultancyFirm/Controllers/HomeController.cs
@@ -10,13 +10,17 @@ namespace TheConsultancyFirm.Controllers
 {
     public class HomeController : Controller
     {
-        /// <summary>
-        /// Haalt met GET de homepagina op
-        /// </summary>
-        /// <returns>De homepagina -> Index.cshtml</returns>
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
