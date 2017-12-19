dee096a6063a4efb1719277700bf9d6d11aa8bed
KevinHoudts <kvhoudts@hotmail.com>
2017-12-19 13:28:57 +0100

    UnitTest
    Created the unit test file

---
+++ TheConsultancyFirm.Tests/Controllers/NewsletterControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TheConsultancyFirm.Controllers;
+using TheConsultancyFirm.Data;
+using TheConsultancyFirm.Models;
+using Xunit;
+using Microsoft.EntityFrameworkCore;
+
+namespace TheConsultancyFirm.Tests.Controllers
+{
+    class NewsletterControllerTest
+    {
+        [Fact]
+        public void Index(string email)
+        {
+
+            //Assert.Equal("info@email.nl", model.Email);
+        }
+    }
+}
