7b9c6d359488c719c358f2e6a7982a55bc7d4730
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-12-19 11:45:25 +0100

    Save contact form in database and show message

--- TheConsultancyFirm/Controllers/ContactController.cs
+++ TheConsultancyFirm/Controllers/ContactController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheConsultancyFirm.Data;
+using TheConsultancyFirm.Models;
 
 namespace TheConsultancyFirm.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public ContactController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(new Contact());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Index([Bind("Name,Email,Mobile,Subject,Message")] Contact contact)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(contact);
+            }
+
+            _context.Add(contact);
+            await _context.SaveChangesAsync();
+
+            TempData["ContactMessage"] = "Je bericht is verzonden.";
+            return Redirect(Url.Action(nameof(Index)) + "#contact");
         }
     }
 }
