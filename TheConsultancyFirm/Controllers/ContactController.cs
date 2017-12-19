e482609ac71b69bf90ff87c644ccf0552b36e23b
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2017-12-19 12:40:29 +0100

    Send notification email after contact form submitted in the background

--- TheConsultancyFirm/Controllers/ContactController.cs
+++ TheConsultancyFirm/Controllers/ContactController.cs
@@ -2,16 +2,19 @@
 using Microsoft.AspNetCore.Mvc;
 using TheConsultancyFirm.Data;
 using TheConsultancyFirm.Models;
+using TheConsultancyFirm.Services;
 
 namespace TheConsultancyFirm.Controllers
 {
     public class ContactController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IMailService _mailService;
 
-        public ContactController(ApplicationDbContext context)
+        public ContactController(ApplicationDbContext context, IMailService mailService)
         {
             _context = context;
+            _mailService = mailService;
         }
 
         public IActionResult Index()
@@ -28,8 +31,12 @@ namespace TheConsultancyFirm.Controllers
                 return View(contact);
             }
 
+            // Save to database
             _context.Add(contact);
             await _context.SaveChangesAsync();
+
+            // Send notification email
+            _mailService.SendContactMailAsync(contact);
 
             TempData["ContactMessage"] = "Je bericht is verzonden.";
             return Redirect(Url.Action(nameof(Index)) + "#contact");
---
+++ TheConsultancyFirm/Models/Mail/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TheConsultancyFirm.Models.Mail
+{
+    public class MailSettings
+    {
+        public string SenderName { get; set; }
+        public string SenderEmail { get; set; }
+        public string EmailPassword { get; set; }
+        public string SmtpHost { get; set; }
+        public int SmtpPort { get; set; }
+    }
+}
