2b912167ce392255a70aa855af56f9cc0af0aca3
koenvzeijl <kvanzeijl@hotmail.com>
2017-12-20 10:11:02 +0100

    Made the tag relation (#26)

---
+++ TheConsultancyFirm/Repositories/ISolutionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using TheConsultancyFirm.Models;
+
+namespace TheConsultancyFirm.Repositories
+{
+	public interface ISolutionRepository
+	{
+		Solution Get(int id);
+		IQueryable<Solution> GetAll();
+	}
+}
--- TheConsultancyFirm/Startup.cs
+++ TheConsultancyFirm/Startup.cs
@@ -24,16 +24,27 @@ namespace TheConsultancyFirm
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
+			//Services
+            services.AddTransient<IRelatedItemsService, RelatedItemsService>();
+            services.AddSingleton<IMailService, MailService>();
+          
+			//Repositories
+            services.AddScoped<ICaseRepository, CaseRepository>();
+            services.AddScoped<ISolutionRepository, SolutionRepository>();
+            services.AddScoped<INewsRepository, NewsRepository>();
+            services.AddScoped<IDownloadRepository, DownloadRepository>();
+            services.AddScoped<IContactRepository, ContactRepository>();
+          
+			services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddSingleton<IMailService, MailService>();
+            
 
-            services.AddScoped<IContactRepository, ContactRepository>();
+            
 
             services.Configure<MailSettings>(Configuration.GetSection("Mail"));
 
