eaa9a7c4e35d79d7ea28846592e7cd302b8b0ffd
Sohaib98 <sohaib.ahmad.112@gmail.com>
2017-12-21 15:16:14 +0100

    DownloadsDashboard finished.

--- TheConsultancyFirm/Models/Download.cs
+++ TheConsultancyFirm/Models/Download.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheConsultancyFirm.Models
 {
@@ -10,7 +13,12 @@ namespace TheConsultancyFirm.Models
         public string Description { get; set; }
         public int AmountOfDownloads { get; set; }
         public DateTime Date { get; set; }
+
+        [Url]
         public string LinkPath { get; set; }
+
+            [NotMapped]
+            public IFormFile File { get; set; }
 
         public List<Block> Blocks { get; set; }
         public List<DownloadTag> DownloadTags { get; set; }
