45ca5e980b0bd8fab18d7db83dc8bdf2636baf11
KevinHoudts <kvhoudts@hotmail.com>
2017-12-21 11:07:58 +0100

    Index berichten
    The index page for the contacts messages. First attemp to show the active item in the sidebar.

---
+++ TheConsultancyFirm/Areas/Dashboard/Controllers/ContactsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TheConsultancyFirm.Data;
+using TheConsultancyFirm.Models;
+
+namespace TheConsultancyFirm.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class ContactsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ContactsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dashboard/Contacts
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "Berichten";
+            return View(await _context.Contacts.ToListAsync());
+        }
+
+        // GET: Dashboard/Contacts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewData["Title"] = "Berichten";
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // GET: Dashboard/Contacts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = await _context.Contacts
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        // POST: Dashboard/Contacts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contact = await _context.Contacts.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Contacts.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContactExists(int id)
+        {
+            return _context.Contacts.Any(e => e.Id == id);
+        }
+    }
+}
--- TheConsultancyFirm/Models/Contact.cs
+++ TheConsultancyFirm/Models/Contact.cs
@@ -7,18 +7,22 @@ namespace TheConsultancyFirm.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Naam")]
         public string Name { get; set; }
 
         [Required, EmailAddress]
         public string Email { get; set; }
 
         [Phone]
+        [Display(Name = "Mobiel")]
         public string Mobile { get; set; }
 
         [Required]
+        [Display(Name = "Onderwerp")]
         public string Subject { get; set; }
 
         [Required]
+        [Display(Name = "Bericht")]
         public string Message { get; set; }
     }
 }
