314c9b914f785881a2a2b84015c986d82c9e44e7
KevinHoudts <kvhoudts@hotmail.com>
2017-12-21 15:33:10 +0100

    Test not working

---
+++ TheConsultancyFirm.Tests/Areas/Dashboard/Controllers/ContactsControllerTest.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using TheConsultancyFirm.Areas.Dashboard.Controllers;
+using TheConsultancyFirm.Models;
+using TheConsultancyFirm.Repositories;
+using Xunit;
+
+namespace TheConsultancyFirm.Tests.Controllers
+{
+    [Area("Dashboard")]
+    public class ContactsControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            var controller = new ContactsController(null);
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(Task.FromResult(result));
+        }
+
+        [Fact]
+        public void Details()
+        {
+            var controller = new ContactsController(null);
+
+            var model = new Contact
+            {
+                Id = 0,
+                Name = "Kevin",
+                Email = "kevin@example.com",
+                Subject = "Anders",
+                Message = "I have a question."
+            };
+
+            var contactRepository = new Mock<IContactRepository>();
+            contactRepository.Setup(repo => repo.AddAsync(model)).Returns(Task.FromResult(0));
+
+            var result = controller.Details(model.Id);
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
