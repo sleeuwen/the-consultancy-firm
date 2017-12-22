1c6797f5a5265a29e0705f0f0851b0bafe5742ad
KevinHoudts <kvhoudts@hotmail.com>
2017-12-22 14:59:03 +0100

    Contact form (#37)
    * Run migrations after successful deployment
    
    * Index berichten
    
    The index page for the contacts messages. First attemp to show the active item in the sidebar.
    
    * Sidenav + Details page
    
    Changed the sidenav so the it show the curretn active page. Changed _context to _repository.
    
    * Added new prop to Contact Model
    
    * Badge shows current unread messages
    
    change badge css to display none
    
    * Changed breadcrumb
    
    * Test not working
    
    * Changed Repository & Unit test
    
    Not working yet.
    
    * Made 6 Unit test for the detaisl action
    
    They all pass :+1:  plus the index unit test. Changed ajax to make it more 'perfect'
    
    * Fixed the test
    
    As requested by michael. I made a constructor wich create a mock so I can reuse that one mock! :+1:
    
    * Little change

---
+++ TheConsultancyFirm/Migrations/20171221115503_AddedReadedProp.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheConsultancyFirm.Migrations
+{
+    public partial class AddedReadedProp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Contacts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contacts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Contacts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Readed",
+                table: "Contacts",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Readed",
+                table: "Contacts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Subject",
+                table: "Contacts",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Contacts",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Contacts",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contacts",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- TheConsultancyFirm/Repositories/IContactRepository.cs
+++ TheConsultancyFirm/Repositories/IContactRepository.cs
@@ -1,10 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using TheConsultancyFirm.Models;
+using System.Collections.Generic;
 
 namespace TheConsultancyFirm.Repositories
 {
     public interface IContactRepository
     {
+        Task<Contact> Get(int id);
+        Task<IEnumerable<Contact>> GetAll();
         Task AddAsync(Contact contact);
+        Task Update(Contact contact);
+        int CountUnreaded();
     }
 }
