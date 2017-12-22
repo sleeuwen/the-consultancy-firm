aefff17f5781673f14e33b6eba16a57a9bccde4e
michaelvs97 <michael_V_S@hotmail.com>
2017-12-22 13:48:16 +0100

    Working on some tests

---
+++ TheConsultancyFirm.Tests/Controllers/SolutionsControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheConsultancyFirm.Areas.Dashboard.Controllers;
+using TheConsultancyFirm.Models;
+using TheConsultancyFirm.Repositories;
+using TheConsultancyFirm.Services;
+using Xunit;
+
+namespace TheConsultancyFirm.Tests.Controllers
+{
+    public class SolutionsControllerTest
+    {
+        private readonly Mock<ISolutionRepository> _solutionRepository;
+        public SolutionsControllerTest()
+        {
+            _solutionRepository = new Mock<ISolutionRepository>();
+        }
+
+        [Fact]
+        public async Task Index()
+        {
+            var controller = new SolutionsController(_solutionRepository.Object);
+            var result = await controller.Index();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public async Task SuccesfulDetailCall()
+        {
+            var model = new Solution
+            {
+                Id = 0,
+                LastModified = DateTime.UtcNow,
+                Title = "Title 1"
+            };
+            
+            _solutionRepository.Setup(repo => repo.Get(0)).Returns(Task.FromResult<Solution>(model));
+                       
+            var controller = new SolutionsController(_solutionRepository.Object);
+            
+            var result = await controller.Details(model.Id);
+            
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Equal(model, viewResult.Model);
+        }
+    
+        [Fact]
+        public async Task FailedDetailCall()
+        {
+            var model = new Solution
+            {
+                Id = 0,
+                LastModified = DateTime.UtcNow,
+                Title = "Title 1"
+            };
+
+            _solutionRepository.Setup(repo => repo.Get(0)).Returns(Task.FromResult<Solution>(null));
+       
+            var controller = new SolutionsController(_solutionRepository.Object);
+            
+            var result = await controller.Details(null);
+            
+            Assert.IsType<NotFoundResult>(result);
+           
+            _solutionRepository.Verify(repo => repo.Update(model), Times.Never);
+        }
+
+    }
+}
