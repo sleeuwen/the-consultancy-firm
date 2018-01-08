3c73b0c489b448b28a59cc6b4bc759a65f7f4cea
Unknown <kvanzeijl@hotmail.com>
2018-01-08 18:35:24 +0100

    WIP

---
+++ TheConsultancyFirm/ViewComponents/SharingViewComponent.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Web;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc;
+using TheConsultancyFirm.Models;
+using TheConsultancyFirm.ViewModels;
+
+namespace TheConsultancyFirm.ViewComponents
+{
+    public class SharingViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(object block)
+        {
+            var model = new SharingViewModel();
+            switch (block)
+            {
+                case Case c:
+                    model.Title = c.Title;
+                    model.Description = HttpUtility.UrlEncode(c.SharingDescription);
+                    model.Link = "/cases/" + c.Id;
+                    model.ImageUrl = c.PhotoPath;
+                    break;
+                case NewsItem n:
+                    model.Title = n.Title;
+                    model.Description = n.SharingDescription;
+                    model.Link = "/newsitems/" + n.Id;
+                    model.ImageUrl = n.PhotoPath;
+                    break;
+                case Solution s:
+                    model.Title = s.Title;
+                    model.Description = s.SharingDescription;
+                    model.Link = "/solutions/" + s.Id;
+                    model.ImageUrl = null;
+                    break;
+            }
+            model.Twitter = $"https://twitter.com/intent/tweet?text={model.Description}";
+            return View(model);
+        }
+    }
+}
