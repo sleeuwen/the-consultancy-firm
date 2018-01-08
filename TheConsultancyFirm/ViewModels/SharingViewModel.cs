8965ce5f707349af524fc925cf8b5230dc7cd0e3
Unknown <kvanzeijl@hotmail.com>
2018-01-08 18:35:24 +0100

    WIP

---
+++ TheConsultancyFirm/ViewModels/SharingViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace TheConsultancyFirm.ViewModels
+{
+    public class SharingViewModel
+    {
+        public string Twitter { get; set; }
+        public string LinkedIn { get; set; }
+        public string Facebook { get; set; }
+        public string Google { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+}
