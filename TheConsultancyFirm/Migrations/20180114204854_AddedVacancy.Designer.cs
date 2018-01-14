73692d117c0fb8029d68eb669fbd886ec52fc2a1
michaelvs97 <michael_V_S@hotmail.com>
2018-01-14 22:41:48 +0100

    Functional vacancies partial (#71)

--- TheConsultancyFirm/Migrations/20180114204854_AddedVacancy.Designer.cs
+++ TheConsultancyFirm/Migrations/20180114204854_AddedVacancy.Designer.cs
@@ -12,9 +12,10 @@ using TheConsultancyFirm.Data;
 namespace TheConsultancyFirm.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180114204854_AddedVacancy")]
+    partial class AddedVacancy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
