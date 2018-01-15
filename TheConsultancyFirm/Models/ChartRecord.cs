9ffc8af4fc918eb07d27c96011855855356cb21d
Unknown <kvanzeijl@hotmail.com>
2018-01-15 15:22:37 +0100

    not mapped

--- TheConsultancyFirm/Models/ChartRecord.cs
+++ TheConsultancyFirm/Models/ChartRecord.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TheConsultancyFirm.Models
 {
+    [NotMapped]
     public class ChartRecord
     {
         public ChartRecord(string date, int visits)
