20d33db3f75e0987712198e3992e8d0b4723ba91
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2018-01-15 08:43:16 +0100

    Rename db table Vacancy to Vacancies

--- TheConsultancyFirm/Migrations/20180115073905_RenameVacancyToVacancies.Designer.cs
+++ TheConsultancyFirm/Migrations/20180115073905_RenameVacancyToVacancies.Designer.cs
@@ -12,9 +12,10 @@ using TheConsultancyFirm.Data;
 namespace TheConsultancyFirm.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180115073905_RenameVacancyToVacancies")]
+    partial class RenameVacancyToVacancies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
