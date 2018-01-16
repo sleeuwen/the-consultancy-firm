3e6a4253c4bae022b5cc867d87ac90ebe3b310a9
michaelvs97 <michael_V_S@hotmail.com>
2018-01-16 10:07:30 +0100

    Dynamic Content

---
+++ TheConsultancyFirm/Migrations/20180116090523_DynamicContentTranslation.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace TheConsultancyFirm.Migrations
+{
+    public partial class DynamicContentTranslation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Solutions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "NewsItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Downloads",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Language",
+                table: "Cases",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ItemTranslations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ContentType = table.Column<int>(nullable: false),
+                    IdEn = table.Column<int>(nullable: false),
+                    IdNl = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ItemTranslations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ItemTranslations");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Solutions");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "NewsItems");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Downloads");
+
+            migrationBuilder.DropColumn(
+                name: "Language",
+                table: "Cases");
+        }
+    }
+}
