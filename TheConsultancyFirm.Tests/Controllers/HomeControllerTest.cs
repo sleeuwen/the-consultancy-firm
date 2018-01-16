ca1eda3f7b634b450842f7b095392922e724e80f
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2018-01-16 16:03:11 +0100

    Fix home controller test

--- TheConsultancyFirm.Tests/Controllers/HomeControllerTest.cs
+++ TheConsultancyFirm.Tests/Controllers/HomeControllerTest.cs
@@ -16,24 +16,31 @@ namespace TheConsultancyFirm.Tests.Controllers
     {
         private Mock<ICustomerRepository> _customerRepository;
         private Mock<INewsItemRepository> _newsItemRepository;
+        private Mock<ISolutionRepository> _solutionRepository;
+        private Mock<ICaseRepository> _caseRepository;
 
         public HomeControllerTest()
         {
             _customerRepository = new Mock<ICustomerRepository>();
             _newsItemRepository = new Mock<INewsItemRepository>();
+            _solutionRepository = new Mock<ISolutionRepository>();
+            _caseRepository = new Mock<ICaseRepository>();
         }
-         
+
         [Fact]
         public async Task Index()
         {
             var model = new List<Customer>();
             var model2 = new List<NewsItem>().AsQueryable().BuildMock();
-
+            var solutions = new List<Solution>().AsQueryable().BuildMock();
+            var cases = new List<Case>();
 
             _customerRepository.Setup(repo => repo.GetAll()).Returns(Task.FromResult<List<Customer>>(model));
             _newsItemRepository.Setup(repo => repo.GetAll()).Returns(model2.Object);
+            _solutionRepository.Setup(repo => repo.GetAll()).Returns(solutions.Object);
+            _caseRepository.Setup(repo => repo.GetHomepageItems()).Returns(Task.FromResult(cases));
 
-            var controller = new HomeController(_customerRepository.Object, _newsItemRepository.Object);
+            var controller = new HomeController(_customerRepository.Object, _newsItemRepository.Object, _solutionRepository.Object, _caseRepository.Object);
             var result = await controller.Index();
 
             Assert.IsType<ViewResult>(result);
