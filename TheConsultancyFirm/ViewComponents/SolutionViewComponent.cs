3111d64abc8771b27a4d2fe4c94671ac1100ca6a
michaelvs97 <michael_V_S@hotmail.com>
2018-01-17 14:26:57 +0100

    WIP

---
+++ TheConsultancyFirm/ViewComponents/SolutionViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TheConsultancyFirm.Repositories;
+
+namespace TheConsultancyFirm.ViewComponents
+{
+    public class SolutionViewComponent : ViewComponent
+    {
+        private readonly ISolutionRepository _solutionRepository;
+
+        public SolutionViewComponent(ISolutionRepository solutionRepository)
+        {
+            _solutionRepository = solutionRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var solutions = _solutionRepository.GetAll().OrderByDescending(c => c.Date).Take(3).ToList();
+
+            return View(solutions);
+        }
+    }
+}
