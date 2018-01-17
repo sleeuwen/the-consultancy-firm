872acd03f97a558729000fb8ca4974f351757b0c
Sohaib98 <sohaib.ahmad.112@gmail.com>
2018-01-17 12:47:45 +0100

    Added test for checking searches with quotes.

---
+++ TheConsultancyFirm.Tests/Controllers/SearchControllerTest.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TheConsultancyFirm.Controllers;
+using TheConsultancyFirm.Repositories;
+using Xunit;
+
+namespace TheConsultancyFirm.Tests.Controllers
+{
+    public class SearchControllerTest
+    {
+        private Mock<ISearchRepository> _searchRepository;
+
+        public SearchControllerTest()
+        {
+            _searchRepository = new Mock<ISearchRepository>();
+        }
+
+        [Fact]
+        public async Task Index()
+        {
+            var controller = new SearchController(_searchRepository.Object);
+            var result = await controller.Index("");
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void NumberWithQuote()
+        {
+            var controller = new SearchController(_searchRepository.Object);
+            string s = "'2";
+            var result = controller.Index(s).IsCompletedSuccessfully;
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void NumberWithQuote2()
+        {
+            var controller = new SearchController(_searchRepository.Object);
+            string s = "2'";
+            var result = controller.Index(s).IsCompletedSuccessfully;
+
+            Assert.True(result);
+        }
+        [Fact]
+        public void Quote()
+        {
+            var controller = new SearchController(_searchRepository.Object);
+            string s = "'";
+            var result = controller.Index(s).IsCompletedSuccessfully;
+
+            Assert.True(result);
+        }
+    }
+}
