02199527ac0b33beccaf61e5f9ed9693c3d49261
michaelvs97 <michael_V_S@hotmail.com>
2018-01-17 14:36:18 +0100

    Working links in footer

--- TheConsultancyFirm/ViewComponents/SolutionViewComponent.cs
+++ TheConsultancyFirm/ViewComponents/SolutionViewComponent.cs
@@ -15,7 +15,8 @@ namespace TheConsultancyFirm.ViewComponents
 
         public IViewComponentResult Invoke()
         {
-            var solutions = _solutionRepository.GetAll().OrderByDescending(c => c.Date).Take(3).ToList();
+            var language = HttpContext?.Request?.Cookies[".AspNetCore.Culture"] == "c=en-US|uic=en-US" ? "en" : "nl";
+            var solutions = _solutionRepository.GetAll().Where(s => s.Enabled && !s.Deleted && s.Language == language).OrderByDescending(s => s.Date).Take(3).ToList();
 
             return View(solutions);
         }
