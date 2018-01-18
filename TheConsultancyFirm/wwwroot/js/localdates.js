265e2481f88394fbad327aea58a1c74c9123f35e
Stephan van Leeuwen <stephan.vanleeuwen5@gmail.com>
2018-01-18 11:17:11 +0100

    Update local time script

--- TheConsultancyFirm/wwwroot/js/localdates.js
+++ TheConsultancyFirm/wwwroot/js/localdates.js
@@ -1,17 +1,6 @@
-var count = document.getElementById("count").value;
-function setLastModifiedDates() {
-    for (i = 0; i < count; i++) {
-        var value = document.getElementsByClassName("date");
-        var date = new Date(value[i].value + ' UTC');
-        document.getElementsByClassName("lastModified")[i].innerHTML = date;
-    }
-}
-function setDates() {
-    for (i = 0; i < count; i++) {
-        var value = document.getElementsByClassName("dateCreated");
-        var date = new Date(value[i].value + ' UTC');
-        document.getElementsByClassName("created")[i].innerHTML = date;
-    }
-}
-setDates();
-setLastModifiedDates();
+jQuery(function ($) {
+    $('time').each(function () {
+        var date = new Date($(this).text() + ' UTC');
+        $(this).text(date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes());
+    });
+});
